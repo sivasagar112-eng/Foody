e517360e3ee8b70ce4320843fcbb613d1e6f01ee
Siva <sivasagar@gmail.com>
2026-07-21 14:04:02 +0530

    Configure NativeWind v4 styling and Metro config for correct Web CSS output compilation

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="nativewind/types" />
+
+declare module "*.css" {
+  const content: any;
+  export default content;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // tailwind.config.js
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
