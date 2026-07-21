5aa4f1f723c4a788dcc5504c73d5dfecb588ca4a
Siva <sivasagar@gmail.com>
2026-07-21 15:11:39 +0530

    Save: update package lock, entry file imports, and app configs

--- index.ts
+++ index.ts
@@ -1,13 +1,7 @@
 import { registerRootComponent } from 'expo';
-import { NativeWindStyleSheet } from 'nativewind';
+import './global.css';
 
 import App from './App';
-
-// Configure NativeWind for Web platform output
-NativeWindStyleSheet.setOutput({
-  default: 'native',
-  web: 'css',
-});
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
